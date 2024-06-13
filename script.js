67bf85b4435d4015f08bdb4bdf3ff1b8ccbb4fb6
jungwonyu <sunsetinmygarden@gmail.com>
2024-06-13 15:06:56 +0900

    refactor: modify css and js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+ const get = (target, parent = document) => parent.querySelector(target);
+ const gets = (target, parent = document) => [...parent.querySelectorAll(target)];
   /* class */
   const CLA_ON = 'on';
   const CLA_INACTIVE = 'inactive';
@@ -9,89 +11,54 @@ document.addEventListener('DOMContentLoaded', () => {
   const ATTR_DATA_IDX = 'data-idx';
 
   /* element */
-  const wrap = document.querySelector('.wrap');
-  const inputs = document.querySelectorAll('input');
-
-  const exampleBtn = document.querySelector('.exampleBtn');
-  const example = document.querySelector('.example');
-  const closeBtn = document.querySelector('.closeBtn');
-
-  /////////////////////////////////////////////////////////////////////////
-
-  /* show example image */
-  exampleBtn.addEventListener('click', () => showExample());
-  closeBtn.addEventListener('click', () => showExample(false));
-  window.addEventListener('click', (e) => {
-    if (e.target !== exampleBtn) showExample(false);
-  });
-
-  /////////////////////////////////////////////////////////////////////////
-  const finalTarget = document.querySelector('.mainTarget .main');
-  const mainInputs = document.querySelectorAll('.mainTarget input');
-  const sections = document.querySelectorAll('.section');
-
+  const wrap = get('.wrap');
+  const inputs = gets('textarea');
+  const exampleBtn = get('.exampleBtn');
+  const example = get('.example');
+  const closeBtn = get('.closeBtn');
+  const finalTarget = get('.mainTarget .main');
+  const mainInputs = gets('.mainTarget textarea');
+  const sections = gets('.section');
+  const saveBtn = get('.saveBtn');
+  const contents = get('.contents');
   const isInputed = () => {
     let bool = false;
     if (finalTarget.value.length > 0) bool = true;
     return bool;
   };
 
-  inputs.forEach((input) => {
-    input.addEventListener('input', () => {
-      if (isInputed()) {
-        if (finalTarget.value.length > 0) {
-          mainInputs.forEach((mainInput) =>
-            mainInput.classList.add(CLA_ACTIVE)
-          );
-        }
-      } else {
-        mainInputs.forEach((mainInput) =>
-          mainInput.classList.remove(CLA_ACTIVE)
-        );
-        finalTarget.classList.add(CLA_ACTIVE);
-        inputs.forEach((input) => (input.value = ''));
-        sections.forEach((section) => section.classList.remove(CLA_ACTIVE));
-      }
-    });
-  });
+  exampleBtn.addEventListener('click', () => showExample());
+  closeBtn.addEventListener('click', () => showExample(false));
+  window.addEventListener('click', (e) => (e.target !== exampleBtn) && showExample(false));
+  saveBtn.addEventListener('click', () => saveImage());
+  inputs.forEach((input) => input.addEventListener('input', () => inputProcess(input)));
+  mainInputs.forEach((mainInput) => mainInput.addEventListener('input', () => mainProcess(mainInput)));
 
-  mainInputs.forEach((mainInput) => {
-    mainInput.addEventListener('input', () => {
-      if (mainInput.classList.contains(CLA_MAIN)) return;
+  mainProcess = (mainInput) => {
+    if (mainInput.classList.contains(CLA_MAIN)) return;
+    const mainInputIdx = Number(mainInput.getAttribute(ATTR_DATA_IDX)) - 1;
+    if (mainInput.value.length > 0) {
+      sections[mainInputIdx].classList.add(CLA_ACTIVE);
+      sections[mainInputIdx].children[4].value = mainInput.value;
+    } else {
+      sections[mainInputIdx].classList.remove(CLA_ACTIVE);
+      for (let i = 0; i < 9; i++) sections[mainInputIdx].children[i].value = '';
+    }
+  }
 
-      const mainInputIdx = Number(mainInput.getAttribute(ATTR_DATA_IDX)) - 1;
-
-      if (mainInput.value.length > 0) {
-        sections[mainInputIdx].classList.add(CLA_ACTIVE);
-        sections[mainInputIdx].children[4].value = mainInput.value;
-      } else {
-        sections[mainInputIdx].classList.remove(CLA_ACTIVE);
-        for (let i = 0; i < 9; i++)
-          sections[mainInputIdx].children[i].value = '';
-      }
-    });
-  });
-
-  /////////////////////////////////////////////////////////////////////////
-  /* save my mandalart */
-  const saveBtn = document.querySelector('.saveBtn');
-  const contents = document.querySelector('.contents');
-
-  saveBtn.addEventListener('click', () => {
-    // html2canvas library
-    // html2canvas(contents).then(canvas => {
-    //   saveImg(canvas.toDataURL('image/jpg'), 'image.jpg');
-    // });
-
-    domtoimage.toPng(contents).then((dataUrl) => {
-        saveImg(dataUrl, 'image.png');
-      })
-      .catch(function (error) {
-        console.error('error', error);
-      });
-  });
-  /* function */
-  function showExample(bool = true) {
+  inputProcess = () => {
+    if (isInputed()) {
+      console.log(finalTarget.value.length)
+      finalTarget.value.length > 0 && mainInputs.forEach((mainInput) => mainInput.classList.add(CLA_ACTIVE));
+    } else {
+      mainInputs.forEach((mainInput) => mainInput.classList.remove(CLA_ACTIVE));
+      finalTarget.classList.add(CLA_ACTIVE);
+      inputs.forEach((input) => (input.value = ''));
+      sections.forEach((section) => section.classList.remove(CLA_ACTIVE));
+    }
+  }
+ 
+  showExample = (bool = true) => {
     if (bool) {
       example.classList.add(CLA_ON);
       wrap.classList.add(CLA_INACTIVE);
@@ -101,12 +68,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function saveImg(uri, filename) {
+  download = (url, filename) => {
     let link = document.createElement('a');
     document.body.appendChild(link);
-    link.href = uri;
+    link.href = url;
     link.download = filename;
     link.click();
     document.body.removeChild(link);
+  }
+
+  saveImage = () => {
+    domtoimage.toPng(contents).then((dataUrl) => {
+      download(dataUrl, 'image.png');
+    })
+    .catch(function (error) {
+      console.error('error', error);
+    });
   }
 });
